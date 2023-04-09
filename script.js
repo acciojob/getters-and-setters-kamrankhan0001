f1805ffab6a32a6020c031eef3ebd27388fcf0a1
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-09 22:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,30 @@
 //complete this code
 class Person {
-	constructor(name , age)
-	{
-		this.name = name;
-		this.age = age;
-	}
-	get getName()
-	{
-		return this.name;
-	}
-	set setage(newAge)
-     setage(newAge)
-	{
-		this.age = newAge;
-	}
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set age(value) {
+    this.age = value;
+  }
 }
 
 
 class Student extends Person {
-	constructor(name , age)
-	{
-		// super.getName(this.name);
-		// super.setage(this.age) = age;
-		super(name,age);
-	}
-	set setage(newAge){
-		super.age = newAge;
-	}
-	study()
-	{
-		console.log(this.name + " is studying");
-	}
+	study() {
+    console.log(this.name + " is studying");
+  }
 }
+
 class Teacher extends Person {
-	constructor(name , age)
-	{
-		super(name , age);
-		// this.age = age;
-	}
-	set setage(newAge){
-		super.age = newAge;
-	}
-	teach()
-	{
-		console.log(this.name + " is teaching");
-	}
+	teach() {
+    console.log(this.name + " is teaching");
+}
 }
 
 
