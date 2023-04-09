df5c6ff13759bc328eb950329acae9da25897cd0
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-09 09:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this._name;
+		this._age;
+	}
+}
+get(){
+	return this._name;
+}
+set(value){
+	return this._age;
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study() {
+    console.log('${this._name}'is studying);
+  }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach() {
+    console.log('${this._name}' is teaching);
+}
 
 // Do not change the code below this line
 window.Person = Person;
